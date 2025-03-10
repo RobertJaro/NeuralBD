62437971c4ff22b496c356e06217f15ff446571b
christoph schirninger <christoph.schirninger@uni-graz.at>
2025-03-10 14:32:09 +0100

    adapt learning rate

--- nstack/train_nbd.py
+++ nstack/train_nbd.py
@@ -8,9 +8,9 @@ from nstack.callback import PlotNeuralBDCallback
 from nstack.nbd import NEURALBDModule
 
 wandb_logger = WandbLogger(project='NeuralBD', entity='christoph-schirninger',
-                           name='neuralbd_gregor', offline=False, dir='/gpfs/data/fs71254/schirni/nstack/Training')
+                           name='neuralbd_gregor_sunspot', offline=False, dir='/cl_tmp/schirnin/Training')
 
-module = NEURALBDModule(n_images=2, dim=512, learning_rate=1e-3, n_modes=44, psf_size=9, muram=False)
+module = NEURALBDModule(n_images=50, dim=512, learning_rate=1e-4, n_modes=44, psf_size=29, muram=False)
 
 image_coordinates = np.stack(np.mgrid[:512, :512], -1)
 coordinates_tensor = torch.from_numpy(image_coordinates).float().view(-1, 2)
@@ -18,7 +18,7 @@ coordinates_tensor = coordinates_tensor / 511.5
 test_coords = coordinates_tensor
 
 plot_callbacks = []
-plot_callbacks += [PlotNeuralBDCallback(2, test_coords)]
+plot_callbacks += [PlotNeuralBDCallback(50, test_coords)]
 #lr_monitor = LearningRateMonitor(logging_interval='step')
 
 n_gpus = torch.cuda.device_count()
@@ -29,6 +29,6 @@ trainer = Trainer(max_epochs=10000,
                   accelerator="gpu" if n_gpus >= 1 else None,
                   num_sanity_val_steps=-1,
                   callbacks=plot_callbacks,
-                  gradient_clip_val=1e-3,
+                  #gradient_clip_val=1e-3,
                   )
 trainer.fit(module)
