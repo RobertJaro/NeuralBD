7407a1fcb23cba4ebeb1bad3ee3f15e40db4889d
christoph schirninger <christoph.schirninger@uni-graz.at>
2025-03-24 13:37:04 +0100

    clean up model implementation

---
+++ nbd/model.py
@@ -0,0 +1,68 @@
+import torch
+from torch import nn
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, in_features, num_freqs=128, min_freq=-2, max_freq=8):
+        super().__init__()
+        frequencies = 2 ** torch.linspace(min_freq, max_freq, num_freqs) * torch.pi
+        self.frequencies = nn.Parameter(frequencies, requires_grad=False)
+        self.d_output = in_features * (1 + num_freqs * 2)
+
+    def forward(self, x):
+        encoded = torch.einsum('...i,j->...ij', x, self.frequencies)
+        encoded = encoded.reshape(*x.shape[:-1], -1)
+        encoded = torch.cat([torch.sin(encoded), torch.cos(encoded), x], -1)
+        return encoded
+
+
+class GaussianPositionalEncoding(nn.Module):
+
+    def __init__(self, d_input, num_freqs=128, scale=2.0 ** 1):
+        super().__init__()
+        frequencies = torch.randn(num_freqs, d_input) * scale
+        self.frequencies = nn.Parameter(2 * torch.pi * frequencies, requires_grad=False)
+        self.d_output = d_input * (num_freqs * 2 + 1)
+
+    def forward(self, x):
+        encoded = torch.einsum('...j,ij->...ij', x, self.frequencies)
+        encoded = encoded.reshape(*x.shape[:-1], -1)
+        encoded = torch.cat([x, torch.sin(encoded), torch.cos(encoded)], -1)
+        return encoded
+
+
+class Sine(nn.Module):
+    def __init__(self, w0=1.):
+        super().__init__()
+        self.w0 = w0
+
+    def forward(self, x):
+        return torch.sin(self.w0 * x)
+
+
+class ImageModel(nn.Module):
+
+    def __init__(self, dim=512, n_channels=2, posencoding=True, posenc_scale=2.0 ** 5):
+        super().__init__()
+        if posencoding:
+            posenc = GaussianPositionalEncoding(2, scale=posenc_scale)
+            # posenc = PositionalEncoding(2, min_freq=0, max_freq=8)
+            d_in = nn.Linear(posenc.d_output, dim)
+            self.d_in = nn.Sequential(posenc, d_in)
+        else:
+            self.d_in = nn.Linear(n_channels, dim)
+
+        lin = [nn.Linear(dim, dim) for _ in range(8)]
+        self.layers = nn.ModuleList(lin)
+        self.d_out = nn.Linear(dim, n_channels)
+        self.activation = Sine()
+
+    def forward(self, coords):
+        x = self.activation(self.d_in(coords))
+
+        for l in self.layers:
+            x = self.activation(l(x))
+        x = self.d_out(x)
+        x = 10 ** x
+        return x
