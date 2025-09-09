95fa57f35dac92e7fde7b417232fd03582bdff4a
christoph schirninger <christoph.schirninger@uni-graz.at>
2025-09-09 14:35:56 +0200

    add loader to load checkpoints for evaluation

---
+++ nbd/evaluation/loader.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+from torch import nn
+from tqdm import tqdm
+
+
+
+class NBDOutput:
+
+    def __init__(self, model_path, device=None):
+        self.device = device if device is not None else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        state = torch.load(model_path, map_location=self.device, weights_only=False)
+
+        self.image_coords = state['image_coords']
+        self.model = state['image_model']
+        self.model = nn.DataParallel(self.model)
+        self.model.eval()
+
+
+    def load(self, coords, batch_size=2048, progress=True):
+        batch_size = batch_size * torch.cuda.device_count() if torch.cuda.is_available() else 1024
+        coordinates_tensor = torch.from_numpy(coords).float().view(-1, 2)
+
+        n_batches = int(np.ceil(coordinates_tensor.shape[0] / batch_size))
+        iter_ = tqdm(range(n_batches)) if progress else range(n_batches)
+        output_image = []
+        for i in iter_:
+            batch_coords = coordinates_tensor[i * batch_size:(i + 1) * batch_size].to(self.device)
+
+            pred = self.model(batch_coords)
+
+            output_image += [pred.detach().cpu().numpy()]
+        output_image = np.concatenate(output_image, axis=0).reshape((coords.shape[0], coords.shape[1], 2))
+
+        return output_image
+
+    def load_reconstructed_img(self, **kwargs):
+        return self.load(self.image_coords)
