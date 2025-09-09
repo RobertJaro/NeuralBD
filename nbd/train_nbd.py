355d234310fee3e88d50bdd7cbe4a3e1bde74e79
christoph schirninger <christoph.schirninger@uni-graz.at>
2025-09-09 14:41:20 +0200

    add the option to select psf type and DKIST

--- nbd/train_nbd.py
+++ nbd/train_nbd.py
@@ -4,7 +4,7 @@ import os
 import torch
 import yaml
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks import LearningRateMonitor, LambdaCallback, ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 
 from nbd.data_module import NeuralBDDataModule
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         neuralbd = NEURALBDModule(
             images_shape=[data_config['crop_size'], data_config['crop_size'], data_config['n_images'], 2],
             pixel_per_ds=data_config['pixel_per_ds'], weights=data_module.contrast_weights, speckle=data_module.speckle,
-            sampling=data_config['psf_type'],
+            sampling=data_config['psf_type'], psf_type=data_config['psf_type'],
             **config['model'])
 
     elif data_config['type'] == 'MURAM':
@@ -60,22 +60,48 @@ if __name__ == '__main__':
             pixel_per_ds=data_config['pixel_per_ds'],
             muram=data_module.muram, psf=data_module.psfs,
             sampling=data_config['psf_type'],
+            psf_type=data_config['psf_type'],
             **config['model'])
+
+    elif data_config['type'] == 'DKIST':
+        neuralbd = NEURALBDModule(
+            images_shape=[data_config['crop_size'], data_config['crop_size'], data_config['n_images'], 2],
+            pixel_per_ds=data_config['pixel_per_ds'],
+            sampling=data_config['psf_type'], psf_type=data_config['psf_type'],
+            **config['model'])
+
     else:
         raise ValueError('Unknown data type')
+
+    checkpoint_callback = ModelCheckpoint(dirpath=base_dir,
+                                          every_n_epochs=training_config['checkpoint_every_n_epochs'] if 'checkpoint_every_n_epochs' in training_config else 5,
+                                          save_last=True)
 
     # Callbacks
     lr_monitor = LearningRateMonitor(logging_interval='step')
 
+    # save callback
+    save_path = os.path.join(base_dir, 'neuralbd.nbd')
+
+    def save(*args, **kwargs):
+        torch.save({
+            'image_model': neuralbd.image_model,
+            'image_coords': data_module.img_coords,
+        }, save_path)
+
+
+    save_callback = LambdaCallback(on_validation_epoch_end=save)
+
     # Train
-    N_GPUS = torch.cuda.device_count()
+    torch.set_float32_matmul_precision('medium')
+    N_GPUS = torch.cuda.device_count() if torch.cuda.is_available() else 1
     trainer = Trainer(max_epochs=epochs,
                       logger=logger,
                       devices=N_GPUS,
                       accelerator='gpu' if N_GPUS >= 1 else None,
                       strategy='dp' if N_GPUS > 1 else None,  # ddp breaks memory and wandb
                       num_sanity_val_steps=-1,
-                      check_val_every_n_epoch=20,
-                      callbacks=[lr_monitor])
+                      check_val_every_n_epoch=10,
+                      callbacks=[lr_monitor, checkpoint_callback, save_callback],)
     trainer.fit(neuralbd, data_module, ckpt_path=ckpt_path)
-    trainer.save_checkpoint(os.path.join(base_dir, 'final.ckpt'))
+    #trainer.save_checkpoint(os.path.join(base_dir, 'final.ckpt'))
