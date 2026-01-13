fa80b0aab3d6aacf5d02a2ba15cd4a79788a968f
christoph schirninger <christoph.schirninger@uni-graz.at>
2026-01-13 11:55:38 +0100

    add KSO dataset

--- nbd/data_module.py
+++ nbd/data_module.py
@@ -8,8 +8,8 @@ from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 
-from nbd.data.editor import get_KL_basis, get_KL_wavefront, generate_PSFs, ReadSimulationEditor, get_convolution, \
-    cutout, get_filtered, optimize_shift, shift_image, compute_rms_contrast
+from nbd.data.editor import get_KL_basis, get_KL_wavefront, generate_PSFs, ReadSimulationEditor, cutout, get_filtered, \
+    optimize_shift, shift_image, compute_rms_contrast
 
 
 class NeuralBDDataModule(LightningDataModule):
@@ -25,6 +25,7 @@ class NeuralBDDataModule(LightningDataModule):
             self.contrast_weights = self.train_dataset.image_contrast
             self.img_coords = self.train_dataset.image_coordinates
             self.speckle = self.train_dataset.fits_array_speckle
+            self.image_mean = self.train_dataset.image_mean
 
         elif data_set_type.upper() == 'MURAM':
             self.train_dataset = MURAMDataset(**dataset_config, shuffle=True)
@@ -33,12 +34,21 @@ class NeuralBDDataModule(LightningDataModule):
             self.muram = self.train_dataset.high_quality
             self.psfs = self.train_dataset.kl_psfs
             self.img_coords = self.train_dataset.image_coordinates
+            self.image_mean = self.train_dataset.image_mean
 
         elif data_set_type.upper() == 'DKIST':
             self.train_dataset = DKISTDataset(**dataset_config, shuffle=True)
             self.valid_dataset = DKISTDataset(**dataset_config, shuffle=False)
 
             self.img_coords = self.train_dataset.image_coordinates
+            self.image_mean = self.train_dataset.image_mean
+
+        elif data_set_type.upper() == 'KSO':
+            self.train_dataset = KSODataset(**dataset_config, shuffle=True)
+            self.valid_dataset = KSODataset(**dataset_config, shuffle=False)
+
+            self.img_coords = self.train_dataset.image_coordinates
+            self.image_mean = self.train_dataset.image_mean
 
         else:
             raise ValueError('Unknown data type')
@@ -56,7 +66,6 @@ class MURAMDataset(TensorDataset):
 
     def __init__(self, data_path, n_images, pixel_per_ds, n_modes=44, psf_size=29, coef_range=2, x_crop=300, y_crop=300,
                  crop_size=512, shuffle=True, batch_size=1024, noise=True, **kwargs):
-
         # Generate PSFs
         kl_basis = get_KL_basis(n_modes_max=n_modes, size=psf_size)
         kl_wavefront = get_KL_wavefront(kl_basis, n_modes, n_images, coef_range=coef_range)
@@ -66,21 +75,26 @@ class MURAMDataset(TensorDataset):
         muram = ReadSimulationEditor().call(data_path)
         sim_array = muram.data
 
-        if noise:
-            mu, sigma = sim_array.mean(), sim_array.std()
-            noise = np.random.normal(mu, sigma, sim_array.shape)
-            sim_array += noise
-        else:
-            pass
+        # if noise:
+        #    mu, sigma = sim_array.mean(), sim_array.std()
+        #    noise = np.random.normal(mu, sigma, sim_array.shape)
+        #    sim_array += noise
+        # else:
+        #    pass
 
         vmin_sim, vmax_sim = sim_array.min(), sim_array.max()
         sim_array = (sim_array - vmin_sim) / (vmax_sim - vmin_sim)
 
-        sim_tensor = torch.tensor(sim_array, dtype=torch.float32).unsqueeze(-1)
-        self.high_quality = cutout(sim_tensor[..., None], x_crop, y_crop, crop_size)
+        # sim_tensor = torch.tensor(sim_array, dtype=torch.float32).unsqueeze(-1)
+        # self.high_quality = cutout(sim_tensor[..., None], x_crop, y_crop, crop_size)
+        sim_array = np.stack([sim_array, sim_array], axis=-1)
+        self.high_quality = sim_array
 
         # Convolve images
-        images = get_convolution(self.high_quality, self.kl_psfs, n_images, noise=False)
+        # images = get_convolution(self.high_quality, self.kl_psfs, n_images, noise=False)
+        images = np.load("/gpfs/data/fs71254/schirni/nstack/data/block_psf_convolved_100.npy")
+        images = np.stack([images, images], -1)
+        self.image_mean = np.mean(images, axis=2)
 
         # Normalize images
         # vmin, vmax = images.min(), images.max()
@@ -119,7 +133,7 @@ class GREGORDataset(TensorDataset):
             fits_header.append(fits.getheader(data_path, i))
         h, w = fits_array[0].shape
         fits_array = np.stack(fits_array, -1).reshape((h, w, 100, 2))
-        fits_array = fits_array[..., 1]  # remove the second channel
+        fits_array = fits_array[..., 0]  # remove the second channel
 
         # Apply low-pass filter
         if filter:
@@ -128,7 +142,7 @@ class GREGORDataset(TensorDataset):
             fits_array = fits_array[0]
 
         # apply shift to align images
-        max_shift = 800
+        max_shift = 300
 
         print('Aligning images...')
         with mp.Pool(mp.cpu_count()) as pool:
@@ -176,6 +190,7 @@ class GREGORDataset(TensorDataset):
         fits_array = np.stack([fits_array, fits_array], -1)
 
         images = fits_array
+        self.image_mean = np.mean(images, axis=2)
 
         # Create dataset
         image_coordinates = np.stack(np.mgrid[:images.shape[0], :images.shape[1]], -1)
@@ -210,24 +225,24 @@ class DKISTDataset(TensorDataset):
         dkist_array = dkist_array.transpose(1, 2, 0)  # [h, w, n_images]
 
         # apply shift to align images
-        max_shift = 600
+        # max_shift = 30
 
-        print('Aligning images...')
-        with mp.Pool(mp.cpu_count()) as pool:
-            shifts = list(tqdm(pool.starmap(optimize_shift,
-                                            [(dkist_array[x_crop - max_shift:x_crop + crop_size + max_shift,
-                                            y_crop - max_shift:y_crop + crop_size + max_shift, 0],
-                                              dkist_array[x_crop - max_shift:x_crop + crop_size + max_shift,
-                                              y_crop - max_shift:y_crop + crop_size + max_shift, i])
-                                             for i in range(n_images)]), total=n_images))
+        # print('Aligning images...')
+        # with mp.Pool(mp.cpu_count()) as pool:
+        #    shifts = list(tqdm(pool.starmap(optimize_shift,
+        #                                    [(dkist_array[x_crop - max_shift:x_crop + crop_size + max_shift,
+        #                                    y_crop - max_shift:y_crop + crop_size + max_shift, 0],
+        #                                      dkist_array[x_crop - max_shift:x_crop + crop_size + max_shift,
+        #                                      y_crop - max_shift:y_crop + crop_size + max_shift, i])
+        #                                     for i in range(n_images)]), total=n_images))
 
-        dkist_array = np.stack([shift_image(dkist_array[x_crop - max_shift:x_crop + crop_size + max_shift,
-        y_crop - max_shift:y_crop + crop_size + max_shift, i],
-                                            shifts[i][0][1], shifts[i][0][0]) for i in range(n_images)], -1)
+        # dkist_array = np.stack([shift_image(dkist_array[x_crop - max_shift:x_crop + crop_size + max_shift,
+        # y_crop - max_shift:y_crop + crop_size + max_shift, i],
+        #                                    shifts[i][0][1], shifts[i][0][0]) for i in range(n_images)], -1)
 
         # Crop images
-        dkist_array = cutout(dkist_array[..., None], max_shift, max_shift, crop_size)
-        # fits_array = cutout(fits_array, x_crop, y_crop, crop_size)
+        # dkist_array = cutout(dkist_array[..., None], max_shift, max_shift, crop_size)
+        dkist_array = cutout(dkist_array[..., None], x_crop, y_crop, crop_size)
 
         # Normalize images
         vmin, vmax = dkist_array.min(), dkist_array.max()
@@ -245,6 +260,7 @@ class DKISTDataset(TensorDataset):
         dkist_array = np.stack([dkist_array, dkist_array], -1)
 
         images = dkist_array
+        self.image_mean = np.mean(images, axis=2)
 
         # Create dataset
         image_coordinates = np.stack(np.mgrid[:images.shape[0], :images.shape[1]], -1)
@@ -253,6 +269,52 @@ class DKISTDataset(TensorDataset):
         # apply binning and cropping
         # x_start, x_end, y_start, y_end = crop if crop else (0, -1, 0, -1)
         # image_coordinates = image_coordinates[x_start:x_end:bin, y_start:y_end:bin]
+        coordinates_tensor = torch.from_numpy(self.image_coordinates).float().view(-1, 2)
+        image_tensor = torch.from_numpy(images).float().reshape(-1, n_images, 2)
+
+        if shuffle:
+            r = torch.randperm(len(image_tensor))
+            image_tensor = image_tensor[r]
+            coordinates_tensor = coordinates_tensor[r]
+
+        # split into batches
+        image_tensor = image_tensor.view(-1, batch_size, n_images, 2)
+        coordinates_tensor = coordinates_tensor.view(-1, batch_size, 2)
+
+        super().__init__(image_tensor, coordinates_tensor)
+
+
+class KSODataset(TensorDataset):
+
+    def __init__(self, data_path, n_images, pixel_per_ds,
+                 shuffle=True, batch_size=1024, **kwargs):
+
+        # Load data
+        fits_array = np.load(data_path)
+
+        # Normalize images
+        vmin, vmax = fits_array.min(), fits_array.max()
+        fits_array = (fits_array - vmin) / (vmax - vmin)
+
+        # Compute contrast and sort images
+        rms = [compute_rms_contrast(fits_array[:, :, i]) for i in range(n_images)]
+        highest_indices = [index for index, value in
+                           sorted(enumerate(rms), key=lambda x: x[1], reverse=True)[:n_images]]
+        self.image_contrast = [rms[i] for i in highest_indices]
+        fits_array = np.stack([fits_array[:, :, i] for i in highest_indices], -1)
+
+        # workaround to use the same channel
+        # fits_array[..., 1] = fits_array[..., 0]
+        fits_array = np.stack([fits_array, fits_array], -1)
+
+        images = fits_array
+        self.image_mean = images[:, :, 0, :]
+
+        # Create dataset
+        image_coordinates = np.stack(np.mgrid[:images.shape[0], :images.shape[1]], -1)
+        self.image_coordinates = image_coordinates / pixel_per_ds
+
+        # apply binning and cropping
         coordinates_tensor = torch.from_numpy(self.image_coordinates).float().view(-1, 2)
         image_tensor = torch.from_numpy(images).float().reshape(-1, n_images, 2)
 
